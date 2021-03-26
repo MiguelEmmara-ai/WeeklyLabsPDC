007d9c2d5eb46d00611ba81389c4e949a1c32937
Miguel <miguelemmara123@gmail.com>
2021-03-26 23:25:39 +1300

    add some function write a file

--- src/Task04_1/MathQuiz.java
+++ src/Task04_1/MathQuiz.java
@@ -1,10 +1,6 @@
 package Task04_1;
 
-import Task11_1.Question;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.InputMismatchException;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -22,56 +18,69 @@ import java.util.Scanner;
 public class MathQuiz {
     public static final Scanner scanner = new Scanner(System.in);
     public static final String filePath = "./resources/T04_users.txt";
-    public static LinkedHashMap < String, Integer > data = new LinkedHashMap <> ();
+    public static LinkedHashMap < String, Integer > data = new LinkedHashMap < > ();
 
     public static void main(String[] args) throws FileNotFoundException {
         int userScore = 0;
         int counter = 0;
-        boolean stop = true;
         System.out.println("Math Quiz");
         System.out.println("=========");
 
         readFile(data);
-        System.out.print("\nWhat Is Your Name: ");
+        System.out.print("\nWhat Is Your User Name: ");
         String userName = scanner.nextLine();
 
-        if (data.containsKey(userName)) {
-            userScore = data.get(userName);
-            Questions questions = new Questions(userName,userScore);
-            System.out.println();
-            questions.getData();
-            System.out.println();
-        } else {
-            Questions questions = new Questions(userName,userScore);
-            System.out.println();
-            questions.getData();
-            System.out.println();
-
-            do {
-                if (counter <= 0) {
-                    System.out.print("Start Math Quiz? (y/x): ");
-                } else {
-                    System.out.print("Continue Math Quiz? (y/x): ");
-                }
-                String answer = scanner.nextLine();
-
-                try {
-                    if (answer.equalsIgnoreCase("y")) {
-                        System.out.println();
-                        questions.startQuiz();
-                        counter++;
-                    } else if (answer.equalsIgnoreCase("x")) {
-                        System.out.println("\nMath Quiz Finished!\n");
-                        questions.getData();
-                        stop = false;
-                    } else {
-                        System.out.println("\nWrong answer, Please Type (y/x)!");
-                    }
-                } catch (InputMismatchException | IllegalArgumentException e) {
-                    System.out.println("Wrong answer, Please Type (y/x):");
-                }
-            } while (stop);
+        while (userName.isEmpty() || userName.contains(" ")) {
+            System.out.println("Please enter a valid name!");
+            System.out.print("What Is Your User Name: ");
+            userName = scanner.nextLine();
         }
+
+        String UserNameCapitalized = convert(userName);
+
+        if (data.containsKey(UserNameCapitalized)) {
+            userScore = data.get(UserNameCapitalized);
+            Questions questions = new Questions(UserNameCapitalized, userScore);
+            System.out.println();
+            questions.getData();
+            System.out.println();
+            quizPrompt(counter, true, UserNameCapitalized, questions);
+        } else {
+            Questions questions = new Questions(UserNameCapitalized, userScore);
+            System.out.println();
+            questions.getData();
+            System.out.println();
+            quizPrompt(counter, true, UserNameCapitalized, questions);
+        }
+    }
+
+    private static void quizPrompt(int counter, boolean stop, String userName, Questions questions) throws FileNotFoundException {
+        do {
+            if (counter <= 0) {
+                System.out.print("Start Math Quiz? (y/x): ");
+            } else {
+                System.out.print("Continue Math Quiz? (y/x): ");
+            }
+            String answer = scanner.nextLine();
+
+            try {
+                if (answer.equalsIgnoreCase("y")) {
+                    System.out.println();
+                    questions.startQuiz();
+                    counter++;
+                } else if (answer.equalsIgnoreCase("x")) {
+                    System.out.println("\nMath Quiz Finished!\n");
+                    questions.getData();
+                    data.put(userName, questions.getUserScore());
+                    editFile(data);
+                    stop = false;
+                } else {
+                    System.out.println("\nWrong answer, Please Type (y/x)!");
+                }
+            } catch (InputMismatchException | IllegalArgumentException e) {
+                System.out.println("Wrong answer, Please Type (y/x):");
+            }
+        } while (stop);
     }
 
     private static void readFile(LinkedHashMap < String, Integer > data) throws FileNotFoundException {
@@ -86,9 +95,47 @@ public class MathQuiz {
             data.put(studentFirstName, score);
         }
         scanner2.close();
+    }
 
-        /*System.out.println(filePath + "\n");
-        for (Map.Entry < String, Integer > e: data.entrySet())
-            System.out.println(e.getKey() + " " + e.getValue());*/
+    private static void editFile(LinkedHashMap < String, Integer > data) throws FileNotFoundException {
+        PrintWriter printWriter = new PrintWriter(new FileOutputStream(filePath));
+
+        for (Map.Entry < String, Integer > entry: data.entrySet()) {
+            String k = entry.getKey();
+            Integer v = entry.getValue();
+            printWriter.println(k + " " + v);
+        }
+
+        printWriter.close();
+    }
+
+    public static String convert(String str) {
+
+        // Create a char array of given String
+        char[] ch = str.toCharArray();
+        for (int i = 0; i < str.length(); i++) {
+
+            // If first character of a word is found
+            if (i == 0 && ch[i] != ' ' ||
+                    ch[i] != ' ' && ch[i - 1] == ' ') {
+
+                // If it is in lower-case
+                if (ch[i] >= 'a' && ch[i] <= 'z') {
+
+                    // Convert into Upper-case
+                    ch[i] = (char)(ch[i] - 'a' + 'A');
+                }
+            }
+
+            // If apart from first character
+            // Any one is in Upper-case
+            else if (ch[i] >= 'A' && ch[i] <= 'Z')
+
+                // Convert into Lower-Case
+                ch[i] = (char)(ch[i] + 'a' - 'A');
+        }
+
+        // Convert the char array to equivalent String
+        return new String(ch);
     }
 }
